c852045b968368c124ad3d48be03a84cbd87b834
Sandeep Rawat <sandeep@opstree.com>
2020-05-09 21:51:47 +0530

    Customised message

--- src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
+++ src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
@@ -28,7 +28,7 @@ public class HomeControllerTest {
     public void helloWorld() {
         getRequest("/")
             .assertStatusCode(OK)
-            .assertResponseBody("Hello from GazGeek!");
+            .assertResponseBody("Hello from Opstree, courtsey GazGeek!");
     }
 
     private HelloWorldResponse getRequest(String uri) {
